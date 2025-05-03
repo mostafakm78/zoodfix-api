21008662d062787dcdcd78edbe7fefc2bbd383b9
Mostafa Kamari <mostafamf555@gmail.com>
2025-05-03 19:12:28 +0330

    add11

--- server.js
+++ server.js
@@ -12,6 +12,46 @@ server.use('/image', jsonServer.defaults({ static: path.join(__dirname, 'image')
 // سایر میدلورها (logger, cors و ...)
 server.use(middlewares);
 
+// ارسال داده‌ها به بخش BuyForm و contact
+server.use(jsonServer.bodyParser);
+
+// افزودن داده‌ها به BuyForm
+server.post('/buyform', (req, res) => {
+  const db = router.db;
+  const { namePerson, phone, email, title, comment } = req.body;
+
+  // ایجاد شناسه جدید به صورت خودکار
+  const newBuyForm = {
+    id: Date.now().toString(),
+    namePerson,
+    phone,
+    email,
+    title,
+    comment,
+  };
+
+  db.get('BuyForm').push(newBuyForm).write();
+  res.status(201).json(newBuyForm); // پاسخ موفق
+});
+
+// افزودن داده‌ها به بخش contact
+server.post('/contact', (req, res) => {
+  const db = router.db;
+  const { fullName, phone, title, message } = req.body;
+
+  // ایجاد شناسه جدید به صورت خودکار
+  const newContact = {
+    id: Date.now().toString(),
+    fullName,
+    phone,
+    title,
+    message,
+  };
+
+  db.get('contact').push(newContact).write();
+  res.status(201).json(newContact); // پاسخ موفق
+});
+
 // استفاده از روتر برای API
 server.use(router);
 
