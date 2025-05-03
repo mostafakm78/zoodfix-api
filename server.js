97c04263a3443e16deff1a8080c2fb2b4b315379
Mostafa Kamari <mostafamf555@gmail.com>
2025-05-03 18:59:04 +0330

    add10

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
+// سرو کردن مسیرهای استاتیک برای تصاویر بلاگ و محصول
+server.use('/image', jsonServer.defaults({ static: path.join(__dirname, 'image') }));
+
+// سایر میدلورها (logger, cors و ...)
 server.use(middlewares);
+
+// استفاده از روتر برای API
 server.use(router);
 
-server.listen(port);
+// اجرای سرور
+server.listen(port, () => {
+  console.log(`JSON Server is running at http://localhost:${port}`);
+});
